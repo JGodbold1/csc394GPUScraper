dd75f2c9d93a3079e18a0b30c54ba5b5b882fb0f
David Kulis <dkulis01@gmail.com>
2022-10-12 03:53:07 -0500

    Added backend functionality.

--- Web App/website/views.py
+++ Web App/website/views.py
@@ -3,6 +3,8 @@ from . import get_db_conn
 
 views = Blueprint('views', __name__)
 
+# home page is also the landing page (for when a user logs in)
 @views.route('/')
 def home():
     return render_template('home.html')
+
