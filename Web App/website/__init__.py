6ae9ebaf3b6b1a9e54e8beba10cf93f5b83e7538
David Kulis <dkulis01@gmail.com>
2022-11-03 02:56:38 -0500

    Minor changes and optimizations

--- Web App/website/__init__.py
+++ Web App/website/__init__.py
@@ -6,8 +6,9 @@ from os import path
 # The current database connection is made
 # with the assumption that it does not have
 # a password and default port 5432
-DB_HOST = 'test'
+DB_NAME = 'test'
 DB_USER = 'postgres'
+DEFAULT_ADMIN_PASS = generate_password_hash('allswellthatendswell')
 
 def create_app():
     app = Flask(__name__)
@@ -26,22 +27,81 @@ def create_app():
 
 # connect to the database
 def get_db_conn():
-    conn = psycopg2.connect(dbname=DB_HOST, user=DB_USER)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER)
     return conn
 
 def create_tables():
-    conn = psycopg2.connect(dbname=DB_HOST, user=DB_USER)
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER)
     cur = conn.cursor()
 
-    # create accounts table
+    cur.execute('DROP TABLE IF EXISTS GPUS CASCADE')
+    # create users table
     cur.execute('''
-                CREATE TABLE IF NOT EXISTS users (
-                    id SERIAL PRIMARY KEY,
-                    username VARCHAR(50) UNIQUE NOT NULL,
-                    password VARCHAR(255) NOT NULL,
-                    role VARCHAR(20)
+                CREATE TABLE IF NOT EXISTS USERS (
+                    username    VARCHAR(32)  NOT NULL UNIQUE PRIMARY KEY,
+                    password    VARCHAR(255) NOT NULL,
+                    isAdmin     BOOL DEFAULT FALSE
                 )
                 ''')
+    # create gpu table
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS GPUS (
+                    gpu_id          SERIAL UNIQUE PRIMARY KEY,
+                    store           TEXT,
+                    gpu             TEXT,
+                    manufacturer    TEXT,
+                    memory          SMALLINT,
+                    price           MONEY,
+                    inStock         BOOL,
+                    onSale          BOOL       
+                )
+                ''')
+    # create favorites table
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS FAVORITES (
+                    id          INTEGER,
+                    username    VARCHAR(32),
+                    CONSTRAINT fk_gpu FOREIGN KEY (id) REFERENCES GPUS(gpu_id),
+                    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES USERS(username)
+                )
+                ''')
+
+    cur.execute('''
+                INSERT INTO USERS (username, password, isAdmin)
+                VALUES (%s, %s, %s)
+                ON CONFLICT DO NOTHING
+                ''', ('dkulis', DEFAULT_ADMIN_PASS, True))
+
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Micro Center', 'GTX 3050', 'Asus', 8, 429.99, false,false)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Micro Center', 'GTX 3050', 'MSI', 8, 339.99, true,true)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Micro Center', 'GTX 3050', 'EVGA', 8, 329.99, true,true)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Micro Center', 'GTX 3060', 'Nvidia', 12, 379.99, true,false)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Micro Center', 'GTX 3060', 'Nvidia', 12, 399.99, true,false)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Best Buy', 'GTX 3080', 'Asus', 8, 429.99, false,false)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Best Buy', 'GTX 3080ti', 'MSI', 8, 339.99, true,true)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Best Buy', 'GTX 3070', 'EVGA', 8, 329.99, true,true)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Best Buy', 'GTX 3070 Super', 'Nvidia', 12, 379.99, true,false)''')
+    cur.execute('''
+                INSERT INTO GPUS (store, gpu, manufacturer, memory, price, inStock, onSale) 
+                VALUES('Best Buy', 'GTX 3060', 'EVGA', 12, 399.99, true,false)''')
                 
     cur.close()
     conn.commit()
--- Web App/website/auth.py
+++ Web App/website/auth.py
@@ -4,8 +4,7 @@ from flask import Blueprint, render_template, request, session, redirect, url_fo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
-# connect to the database using credentials contained within main.py
-conn = get_db_conn()
+conn = get_db_conn() # connect to the database using credentials contained within main.py
 
 # User registration
 @auth.route('/register', methods=['GET','POST'])
@@ -19,8 +18,8 @@ def register():
 
         hashed_password = generate_password_hash(password1)
 
-        # check if account exists already
-        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # check if user already exists
+        cur.execute('SELECT * FROM USERS WHERE username = %s', (username,))
         account = cur.fetchone()
         # show errors upon failed validation checks
         if account:
@@ -33,7 +32,7 @@ def register():
             flash('Password must be longer than six characters.', category='error')
         else:
             # add user to database after passing validation checks
-            cur.execute('INSERT INTO users (username, password)'
+            cur.execute('INSERT INTO USERS (username, password)'
                         'VALUES (%s, %s)',
                         (username, hashed_password))
             conn.commit()
@@ -55,14 +54,14 @@ def login():
         session['username'] = request.form['username']
         password = request.form.get('password')
 
-        cur.execute('SELECT * FROM users WHERE username = %s;', (session['username'],))
+        cur.execute('SELECT * FROM USERS WHERE username = %s;', (session['username'],))
         account = cur.fetchone()
         if account:
-            if check_password_hash(account[2], password):
+            if check_password_hash(account[1], password):
                 flash(f'Logged in as {session["username"]}.', category='success')
                 sleep(.3)
                 # check if role is admin
-                if account[3] == 'admin':
+                if account[2] == True:
                     session['username'] = 'admin'
                 return redirect(url_for('views.home'))
             else:
@@ -84,7 +83,7 @@ def logout():
 @auth.route('/admin')
 def admin():
     cur = conn.cursor()
-    cur.execute('SELECT id, username, role FROM users;')
+    cur.execute('SELECT username, isAdmin FROM USERS;')
     users = cur.fetchall()
     return render_template("admin.html", user=users)
 
@@ -97,7 +96,7 @@ def add_user():
         role = request.form['role']
 
         cur = conn.cursor()
-        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
+        cur.execute('SELECT * FROM USERS WHERE username = %s', (username,))
         account = cur.fetchone()
         # show errors upon failed validation checks
         if account:
@@ -106,11 +105,11 @@ def add_user():
             flash('Username must be more than two characters long.', category='error')
         elif len(password) < 6:
             flash('Password must be longer than six characters.', category='error')
-        elif role != 'user' and role != 'admin':
-            flash("Role must either be 'user' or 'admin'.", category='error')
+        elif role != 'true' and role != 'false':
+            flash("Role must either be 'true' or 'false'.", category='error')
         else:
             # add user to database after passing validation checks
-            cur.execute('INSERT INTO users (username, password, role)'
+            cur.execute('INSERT INTO USERS (username, password, isAdmin)'
                         'VALUES (%s,%s,%s)',
                         (username, generate_password_hash(password), role))
             conn.commit()
@@ -119,10 +118,10 @@ def add_user():
         return redirect(url_for('auth.admin'))
 
 # update users in the database
-@auth.route('/update/<string:id>', methods=['POST','GET'])
-def update(id):
+@auth.route('/update/<string:username>', methods=['POST','GET'])
+def update(username):
     cur = conn.cursor()
-    cur.execute('SELECT * FROM users WHERE id = {0}'.format(id))
+    cur.execute('SELECT * FROM USERS WHERE username = %s', (username,))
     data = cur.fetchall()
     print(data[0])
 
@@ -135,14 +134,13 @@ def update(id):
             flash('Username must be more than two characters long.', category='error')
         elif len(password) < 6:
             flash('Password must be longer than six characters.', category='error')
-        elif role != 'user' and role != 'admin':
-            flash("Role must either be 'user' or 'admin'.", category='error')
+        elif role != 'true' and role != 'false':
+            flash("Role must be set to true or false.", category='error')
         else:
             cur.execute('''
-                        UPDATE users u SET
-                        username = %s, password = %s, role = %s
-                        WHERE id = %s
-                        ''', (username, generate_password_hash(password), role, id))
+                        UPDATE USERS u SET
+                        username = %s, password = %s, isAdmin = %s
+                        ''', (username, generate_password_hash(password), role))
             conn.commit()
             flash('User updated.',category='success')
             return redirect(url_for('auth.admin'))
@@ -150,11 +148,11 @@ def update(id):
     return render_template('update.html', user=data[0])
 
 # remove users from the database
-@auth.route('/delete/<string:id>', methods=['POST','GET'])
-def delete(id):
+@auth.route('/delete/<string:username>', methods=['POST','GET'])
+def delete(username):
     cur = conn.cursor()
 
-    cur.execute('DELETE FROM users WHERE id = {0}'.format(id))
+    cur.execute('DELETE FROM USERS WHERE username = %s', (username,))
     conn.commit()
     flash('User deleted.', category='error')
     return redirect(url_for('auth.admin'))
